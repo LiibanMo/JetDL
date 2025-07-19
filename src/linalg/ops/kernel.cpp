cdf7713bdc32e47fe53d543c754b2c0fc01d7054
LiibanMo <liibanmo12@gmail.com>
2025-07-19 21:30:43 +0100

    removed omp headers

--- src/linalg/ops/kernel.cpp
+++ src/linalg/ops/kernel.cpp
@@ -1,5 +1,4 @@
 #include "kernel.hpp"
-#include <omp.h>
 
 void c_add_cpu(const float* a, const float* b, float* c, const int* shA, const int* shB, int* stA, int* stB, const int SIZE, const int NDIMA, const int NDIMB) {
     const int MAX_NDIM = (NDIMA > NDIMB) ? NDIMA : NDIMB;
